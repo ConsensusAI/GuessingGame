fb8baed72a3f12679424c5554532850c3bec73eb
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-18 12:20:04 -0400

    Implement get rounds for game test

--- GuessTheNumber/src/test/java/com/sg/guessthenumber/data/RoundDatabaseDaoTest.java
+++ GuessTheNumber/src/test/java/com/sg/guessthenumber/data/RoundDatabaseDaoTest.java
@@ -54,7 +54,7 @@ public class RoundDatabaseDaoTest extends TestCase {
         Round round2 = new Round();
         round2.setGameId(game.getId());
         round2.setGuess(1937);
-        round2.setResult("e:p:e:0");
+        round2.setResult("e:0:e:0");
         round2 = roundDao.addRound(round2);
 
         List<Round> rounds = roundDao.getAllRounds();
@@ -127,5 +127,37 @@ public class RoundDatabaseDaoTest extends TestCase {
 
     @Test
     public void testGetRoundsForGame() {
+        Game game = new Game();
+        game.setAnswer(1234);
+        game = gameDao.createGame(game);
+
+        Game game2 = new Game();
+        game2.setAnswer(6789);
+        game2 = gameDao.createGame(game2);
+
+        Round round = new Round();
+        round.setGameId(game.getId());
+        round.setGuess(1435);
+        round.setResult("e:p:e:0");
+        round = roundDao.addRound(round);
+
+        Round round2 = new Round();
+        round2.setGameId(game2.getId());
+        round2.setGuess(8731);
+        round2.setResult("p:e:0:0");
+        round2 = roundDao.addRound(round2);
+
+        Round round3 = new Round();
+        round3.setGameId(game.getId());
+        round3.setGuess(1937);
+        round3.setResult("e:0:e:0");
+        round3 = roundDao.addRound(round3);
+
+        List<Round> roundsForGame = roundDao.getRoundsForGame(game.getId());
+
+        assertEquals(2, roundsForGame.size());
+        assertTrue(roundsForGame.contains(round));
+        assertTrue(roundsForGame.contains(round3));
+        assertFalse(roundsForGame.contains(round2));
     }
 }
