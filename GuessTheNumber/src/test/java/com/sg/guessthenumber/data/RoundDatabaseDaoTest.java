359ee959edb823454636ad794ba7f7cc380e960d
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-18 12:12:31 -0400

    Implement get all rounds method test

--- GuessTheNumber/src/test/java/com/sg/guessthenumber/data/RoundDatabaseDaoTest.java
+++ GuessTheNumber/src/test/java/com/sg/guessthenumber/data/RoundDatabaseDaoTest.java
@@ -13,6 +13,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class)
 public class RoundDatabaseDaoTest extends TestCase {
@@ -38,6 +41,27 @@ public class RoundDatabaseDaoTest extends TestCase {
 
     @Test
     public void testGetAllRounds() {
+        Game game = new Game();
+        game.setAnswer(1234);
+        game = gameDao.createGame(game);
+
+        Round round = new Round();
+        round.setGameId(game.getId());
+        round.setGuess(1435);
+        round.setResult("e:p:e:0");
+        round = roundDao.addRound(round);
+
+        Round round2 = new Round();
+        round2.setGameId(game.getId());
+        round2.setGuess(1937);
+        round2.setResult("e:p:e:0");
+        round2 = roundDao.addRound(round2);
+
+        List<Round> rounds = roundDao.getAllRounds();
+
+        assertEquals(2, rounds.size());
+        assertTrue(rounds.contains(round));
+        assertTrue(rounds.contains(round2));
     }
 
     @Test
@@ -53,9 +77,6 @@ public class RoundDatabaseDaoTest extends TestCase {
         round = roundDao.addRound(round);
 
         Round fromDao = roundDao.getRoundById(round.getId());
-
-        System.out.println(round.getTime());
-        System.out.println(round.getTime());
 
         assertEquals(round, fromDao);
     }
